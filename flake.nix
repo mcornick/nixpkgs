f42cb3b7d6961c28d1ab1ea70b754d0d72ec8369
Mark Cornick <mcornick@mcornick.com>
2025-06-03 17:59:07 -0400

    Resuscitating flake.nix
    Now that I'm using NixOS and flakes I want this to work.

---
+++ flake.nix
@@ -0,0 +1,22 @@
+{
+  description = "Mark's nixpkgs repository";
+  inputs.nixpkgs.url = "github:mcornick/nixpkgs";
+  outputs = { self, nixpkgs }:
+    let
+      systems = [
+        "i686-linux"
+        "x86_64-linux"
+        "armv6l-linux"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "aarch64-darwin"
+      ];
+      forAllSystems = f: nixpkgs.lib.genAttrs systems (system: f system);
+    in
+    {
+      legacyPackages = forAllSystems (system: import ./default.nix {
+        pkgs = import nixpkgs { inherit system; };
+      });
+      packages = forAllSystems (system: nixpkgs.lib.filterAttrs (_: v: nixpkgs.lib.isDerivation v) self.legacyPackages.${system});
+    };
+}
